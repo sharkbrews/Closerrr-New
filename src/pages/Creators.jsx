c0beb531af19f0bf2ac89e53aa8a69c0cd78b4a9
sharkbrews <sharkbrewsinternational@gmail.com>
2026-06-21 16:41:21 +0530

    Optimize layout spacing on mobile for Creators page

--- src/pages/Creators.jsx
+++ src/pages/Creators.jsx
@@ -34,7 +34,7 @@ function Creators() {
     <div className="flex-1 min-h-screen scroll-smooth hide-scrollbar">
       <div className="w-full min-h-screen flex flex-col ">
         <motion.div
-          className="w-full min-h-screen flex flex-col items-center justify-center mt-6 gap-[3.3vmax]"
+          className="w-full min-h-fit md:min-h-screen flex flex-col items-center justify-center pt-24 md:pt-0 pb-10 md:pb-0 mt-6 gap-[3.3vmax]"
           initial="initial"
           animate="animate"
           variants={staggerContainer}
