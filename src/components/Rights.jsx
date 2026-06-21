effd95da51c56b4bdbea66f339d20dbd5aaad870
sharkbrews <sharkbrewsinternational@gmail.com>
2026-06-21 15:48:58 +0530

    Update copyright year to 2026 in Rights.jsx

--- src/components/Rights.jsx
+++ src/components/Rights.jsx
@@ -2,7 +2,7 @@ function Rights() {
   return (
     <div className="w-full flex overflow-x-hidden text-sm py-1.5 justify-center items-center">
       <h1 className="text-customPurple-500  font-hellix-medium ">
-        © 2025 Closerrr Interactive Pvt. Ltd.
+        © 2026 Closerrr Interactive Pvt. Ltd.
       </h1>
     </div>
   );
